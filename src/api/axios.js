66734b0875ac3440e61d6bff2bf50f72540cf8d9
Alok Maurya <alok.maurya@w3villa.com>
2023-09-12 19:34:53 +0530

    api url fixed

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://portfolio-alokm.netlify.com/Data",
+    baseURL: "https://portfolio-alokm.netlify.com/Data",
     headers: {
       "Content-type": "application/json"
     }
